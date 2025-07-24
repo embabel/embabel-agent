391df199346f733ddb8d70fbe5e98ef011a64231
pambrose <pambrose@mac.com>
2025-07-23 19:55:37 -0700

    Refactor logger info message in OptimizingGoapPlanner for improved formatting and readability

--- embabel-agent-api/src/main/kotlin/com/embabel/plan/goap/OptimizingGoapPlanner.kt
+++ embabel-agent-api/src/main/kotlin/com/embabel/plan/goap/OptimizingGoapPlanner.kt
@@ -65,9 +65,12 @@ abstract class OptimizingGoapPlanner(
     override fun prune(planningSystem: GoapPlanningSystem): GoapPlanningSystem {
         val allPlans = plansToGoals(planningSystem)
         loggerFor<OptimizingGoapPlanner>().info(
-            "{} plans to consider in pruning:\n{}",
+            "{} plans to consider in pruning:{}",
             allPlans.size,
-            allPlans.joinToString("\n") { it.infoString(false, 1) }
+            if (allPlans.isEmpty())
+                ""
+            else
+                "\n" + allPlans.joinToString("\n") { it.infoString(false, 1) }
         )
         return planningSystem.copy(
             actions = planningSystem.actions.filter { action ->
