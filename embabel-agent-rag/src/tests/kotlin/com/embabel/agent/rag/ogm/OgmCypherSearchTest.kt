a3ae598fd2d413d0ed6426c9a9d27d804973812b
jasper blues <jasper@liberation-data.com>
2025-08-14 15:20:23 +1000

    Add half of test config.

--- embabel-agent-rag/src/tests/kotlin/com/embabel/agent/rag/ogm/OgmCypherSearchTest.kt
+++ embabel-agent-rag/src/tests/kotlin/com/embabel/agent/rag/ogm/OgmCypherSearchTest.kt
@@ -1,19 +1,66 @@
 package com.embabel.agent.rag.ogm
 
-import com.embabel.agent.rag.TestNeo4jConfig
+import com.embabel.agent.rag.NeoIntegrationTestSupport
 import com.embabel.agent.rag.neo.ogm.OgmCypherSearch
+import com.embabel.agent.spi.Ranking
+import com.embabel.agent.spi.Rankings
+import com.embabel.agent.testing.integration.FakeRanker
+import com.embabel.common.core.types.Described
+import com.embabel.common.core.types.Named
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest
-import org.springframework.test.context.ContextConfiguration
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.Primary
+import org.springframework.test.context.ActiveProfiles
+
+@TestConfiguration
+class FakeConfig {
+
+    @Bean
+    @Primary
+    fun fakeRanker() = object : FakeRanker {
+
+        override fun <T> rank(
+            description: String,
+            userInput: String,
+            rankables: Collection<T>,
+        ): Rankings<T> where T : Named, T : Described {
+            when (description) {
+                "agent" -> {
+                    val a = rankables.firstOrNull { it.name.contains("Star") } ?: fail { "No agent with Star found" }
+                    return Rankings(
+                        rankings = listOf(Ranking(a, .9))
+                    )
+                }
+
+                "goal" -> {
+                    val g =
+                        rankables.firstOrNull { it.description.contains("horoscope") } ?: fail("No goal with horoscope")
+                    return Rankings(
+                        rankings = listOf(Ranking(g, .9))
+                    )
+                }
+
+                else -> throw IllegalArgumentException("Unknown description $description")
+            }
+        }
+    }
+
+}
 
 
-@DataNeo4jTest
-@ContextConfiguration(classes = [TestNeo4jConfig::class])
-class OgmCypherSearchTest {
-
-    @Autowired private lateinit var ogmCypherSearch: OgmCypherSearch
+@ActiveProfiles("test")
+@Import(
+    value = [
+        FakeConfig::class,
+    ]
+)
+class OgmCypherSearchTest(@Autowired private val ogmCypherSearch: OgmCypherSearch)
+    : NeoIntegrationTestSupport() {
 
     @Test
     fun should_query() {
