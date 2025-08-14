cee3408c324aba2fcfccfc511573b98e46c991de
jasper blues <jasper@liberation-data.com>
2025-08-14 13:38:22 +1000

    Add Neo4j Test Container

---
+++ embabel-agent-rag/src/tests/kotlin/com/embabel/agent/rag/ogm/OgmCypherSearchTest.kt
@@ -0,0 +1,28 @@
+package com.embabel.agent.rag.ogm
+
+import com.embabel.agent.rag.TestNeo4jConfig
+import com.embabel.agent.rag.neo.ogm.OgmCypherSearch
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest
+import org.springframework.test.context.ContextConfiguration
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ser.impl.PropertySerializerMap.emptyMap
+
+
+@DataNeo4jTest
+@ContextConfiguration(classes = [TestNeo4jConfig::class])
+class OgmCypherSearchTest {
+
+    @Autowired private lateinit var ogmCypherSearch: OgmCypherSearch
+
+    @Test
+    fun should_query() {
+        val query = "match (n) return n limit 100"
+        val params : Map<String, *> = emptyMap<String, Any>()
+        val result = ogmCypherSearch.query("purpose", query, params, null, null)
+        println("Got result: $result")
+        assertNotNull(result)
+    }
+}
