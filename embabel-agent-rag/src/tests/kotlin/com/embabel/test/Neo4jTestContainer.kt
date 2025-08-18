0678364f04241f4517220cd90f77bc42947cab90
jasper blues <jasper@liberation-data.com>
2025-08-18 11:50:37 +1000

    Clean up.

--- embabel-agent-rag/src/tests/kotlin/com/embabel/test/Neo4jTestContainer.kt
+++ embabel-agent-rag/src/tests/kotlin/com/embabel/test/Neo4jTestContainer.kt
@@ -12,20 +12,8 @@ class Neo4jTestContainer : Neo4jContainer<Neo4jTestContainer> {
         val instance: Neo4jTestContainer = Neo4jTestContainer("neo4j:5.26.1-enterprise")
             .apply {
                 // Resolve the JAR in the user's ~/.m2 repository
-                val apocJar = Paths.get(
-                    System.getProperty("user.home"),
-                    ".m2",
-                    "repository",
-                    "org",
-                    "neo4j",
-                    "procedure",
-                    "apoc-extended",
-                    "5.26.0",
-                    "apoc-extended-5.26.0.jar"
-                ).toString()
-
                 // Mount it inside the container's plugins directory
-                withFileSystemBind(apocJar, "/plugins/apoc-extended-5.26.0.jar")
+                withFileSystemBind(apocJarPath(), "/plugins/apoc-extended-5.26.0.jar")
             }
             .withNeo4jConfig("dbms.security.procedures.unrestricted", "apoc.*")
             .withNeo4jConfig("dbms.logs.query.enabled", "INFO")
@@ -45,6 +33,22 @@ class Neo4jTestContainer : Neo4jContainer<Neo4jTestContainer> {
                 MountableFile.forClasspathResource("reference-data.cypher"),
                 "/var/lib/neo4j/import/reference-data.cypher"
             )
+
+        //TODO: We might be able to clean this up using copyFileToContainer and a classpath resource
+        private fun apocJarPath(): String {
+            val apocJar = Paths.get(
+                System.getProperty("user.home"),
+                ".m2",
+                "repository",
+                "org",
+                "neo4j",
+                "procedure",
+                "apoc-extended",
+                "5.26.0",
+                "apoc-extended-5.26.0.jar"
+            ).toString()
+            return apocJar
+        }
 
 
         init {
