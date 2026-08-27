64bb6f820659ce6b73b51da7b175d9ccb79a8297
deleSerna <nadeeshtv@gmail.com>
2026-08-27 20:11:52 +0200

    Adapted the long method name and added better docs around it
    Signed-off-by: deleSerna <nadeeshtv@gmail.com>

--- embabel-agent-api/src/main/kotlin/com/embabel/agent/spi/validation/agentMetadataReaderValidationUtils.kt
+++ embabel-agent-api/src/main/kotlin/com/embabel/agent/spi/validation/agentMetadataReaderValidationUtils.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024-2026 Embabel Pty Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.embabel.agent.spi.validation
 
 import com.embabel.agent.api.annotation.Action
@@ -27,22 +42,23 @@ fun isActionMethod(
             ReflectionUtils.findMethod(agentClass, method.name, *method.parameterTypes) != null &&
              // Check whether given method can be deserialized.
             (!method.returnType.isInterface || !requireInterfaceDeserializationAnnotations ||
-             hasRequiredJsonDeserializeAnnotationOnInterfaceReturnType(method, logger))
+             isReturnTypeDeSerializable(method, logger))
 }
 
 /**
- * Checks if a method returning an interface returns a type with a @JsonDeserialize annotation.
+ * Checks if the return type of the method is annotated with @JsonDeserialize or @JsonTypeInfo
+ * so that it can be deserialized.
  * @param method The Java method to check.
- * @return true if the return type has a @JsonDeserialize annotation, false otherwise.
+ * @return true if the return type has @JsonDeserialize/@JsonTypeInfo annotation, false otherwise.
  */
-private fun hasRequiredJsonDeserializeAnnotationOnInterfaceReturnType(
+private fun isReturnTypeDeSerializable(
     method: Method,
     logger: Logger): Boolean {
     val hasRequiredAnnotation = AnnotationUtils.findAnnotation(method.returnType, JsonDeserialize::class.java) != null ||
             AnnotationUtils.findAnnotation(method.returnType, JsonTypeInfo::class.java) != null
     if (!hasRequiredAnnotation) {
         logger.warn(
-            "Interface {} used as return type of {}.{} must have @JsonDeserialize or @JsonTypeInfo annotation.",
+            "Return type {} of {}.{} must have @JsonDeserialize or @JsonTypeInfo annotation so that it can be deserialized.",
             method.returnType.name,
             method.declaringClass.name,
             method.name,
